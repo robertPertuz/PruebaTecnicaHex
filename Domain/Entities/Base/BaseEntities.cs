8010fafaed3c5c3164ce0bc25de89614e6663a1b
robertPertuz <ralbertopertuz@unicesar.edu.co>
2024-03-25 00:29:58 -0500

    Agregar archivos de proyecto.

---
+++ Domain/Entities/Base/BaseEntities.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+
+namespace Domain.Entities.Base
+{
+     public class BaseEntities
+    {
+        public virtual string Id { get; set; }
+        private  DateTime creationDate { get; set; }
+    }
+}
---
+++ Infrastucture/Suppliers.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastucture
+{
+    public class Suppliers
+    {
+
+    }
+}
